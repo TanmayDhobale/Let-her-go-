bcc1c5e55d0afdf45ae37d5dd0688bc6e401d077
tanmay4l <89733575+TanmayDhobale@users.noreply.github.com>
2023-12-28 23:53:30 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,12 +2,11 @@
 var config = {
     lines: [
         "Dear stfu,",
-        "i never knew how much you mean to me when we first started talking . until today,",
-        " i still love you as much as i did since the very first day we met. every since you came into my life,",
-        "i felt like i was the happiest . i just want to let you know how much you mean to me and how much i appreciate you.",
-        "you never failed to make me smile. things aren't getting better every since we fought, i just miss you so much. having random break downs,",
-        "with you on my mind wondering if you're doing fine. im sorry i wasn't what you wanted, im sorry i wasn't the best, im sorry i couldn't be good enough for you.", 
-       "i just hope everything will be okay soon, i really miss how we used to talk.🥹"
+        "i never knew how much you mean to me when we first started talking . until today,i still love you as much as i did since the very first day we met. ",
+        " every since you came into my life, i felt like i was the happiest. i just want to let you know how much you mean to me and how much i appreciate you.",
+        "you never failed to make me smile. things aren't getting better every since we fought, i just miss you so much. having random break downs, with you on my mind wondering if you're doing fine. im sorry i wasn't what you wanted,",
+        "im sorry i wasn't the best, im sorry i couldn't be good enough for you.i just hope everything will be okay soon, i really miss how we used to talk.🥹", 
+       
     ],
     date:"2022-08-09T20:15:00",
     names: ["☃︎【 Your Tanmay 】","‎ "]
